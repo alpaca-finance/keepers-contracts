664aac2d7d44c21fb1be88163f34f6f7ec3bd9f8
jr-alpaca <uviveramrom@gmail.com>
2023-12-12 15:50:45 +0700

    chore: add unit test

--- tests/RevenueTreasuryKeepers02.test.ts
+++ tests/RevenueTreasuryKeepers02.test.ts
@@ -11,25 +11,33 @@ import {
 import { FakeContract, smock } from "@defi-wonderland/smock";
 import { solidity } from "ethereum-waffle";
 import { BigNumber } from "ethers";
+import { advanceBlock, increaseTimestamp } from "./utils/time";
 
 chai.use(solidity);
 chai.should();
 chai.use(smock.matchers);
 const { expect } = chai;
 
-describe.only("#RevenueTreasury02", () => {
+describe("#RevenueTreasury02", () => {
   const TRIGGER_WEI = ethers.utils.parseEther("1");
   const TIME_LIMIT = "3600";
 
   let deployer: SignerWithAddress;
 
-  let fakeToken: FakeContract<IERC20>;
+  let fakeUSDT: FakeContract<IERC20>;
+  let fakeALPACA: FakeContract<IERC20>;
   let fakeRevenueTreasury02: FakeContract<IRevenueTreasury02>;
   let fakeTreasuryBuybackStrategy: FakeContract<ITreasuryBuybackStrategy>;
   let keepers02: RevenueTreasuryKeepers02;
 
+  const TreasuryBuybackAction: Record<"INITIATE" | "TERMINATE", BigNumber> = {
+    INITIATE: BigNumber.from(0),
+    TERMINATE: BigNumber.from(1),
+  };
+
   async function fixture() {
-    fakeToken = await smock.fake("IERC20");
+    fakeUSDT = await smock.fake("IERC20");
+    fakeALPACA = await smock.fake("IERC20");
     fakeRevenueTreasury02 = await smock.fake("IRevenueTreasury02");
     fakeTreasuryBuybackStrategy = await smock.fake("ITreasuryBuybackStrategy");
 
@@ -43,10 +51,15 @@ describe.only("#RevenueTreasury02", () => {
       TIME_LIMIT
     );
 
-    fakeRevenueTreasury02.token.returns(fakeToken.address);
+    fakeRevenueTreasury02.token.returns(fakeUSDT.address);
+
     fakeRevenueTreasury02.treasuryBuybackStrategy.returns(
       fakeTreasuryBuybackStrategy.address
     );
+
+    fakeTreasuryBuybackStrategy.token0.returns(fakeUSDT.address);
+    fakeTreasuryBuybackStrategy.token1.returns(fakeALPACA.address);
+    fakeTreasuryBuybackStrategy.accumToken.returns(fakeALPACA.address);
     fakeTreasuryBuybackStrategy.nftTokenId.returns(BigNumber.from(0));
   }
 
@@ -104,7 +117,7 @@ describe.only("#RevenueTreasury02", () => {
     context("when nftTokenId is 0", async () => {
       context("when pendingAlpaca NOT pass triggerWei", async () => {
         it("should return false", async () => {
-          fakeToken.balanceOf.returns(TRIGGER_WEI.sub(1));
+          fakeUSDT.balanceOf.returns(TRIGGER_WEI.sub(1));
           const upKeepStatus = await keepers02.checkUpkeep("0x");
           expect(upKeepStatus[0]).to.be.false;
         });
@@ -112,74 +125,106 @@ describe.only("#RevenueTreasury02", () => {
 
       context("when pendingAlpaca more than triggerWei", async () => {
         it("should return true", async () => {
-          fakeToken.balanceOf.returns(TRIGGER_WEI.add(1));
+          fakeUSDT.balanceOf.returns(TRIGGER_WEI.add(1));
           const upKeepStatus = await keepers02.checkUpkeep("0x");
-          console.log("upkeep", upKeepStatus[1]);
           expect(upKeepStatus[0]).to.be.true;
-          expect(upKeepStatus[1]).to.eq(BigNumber.from(0));
+          expect(upKeepStatus[1]).to.eq(TreasuryBuybackAction.INITIATE);
+        });
+      });
+
+      context("when nftTokenId is not 0", async () => {
+        beforeEach(async () => {
+          await keepers02.performUpkeep(
+            ethers.utils.defaultAbiCoder.encode(
+              ["uint256"],
+              [TreasuryBuybackAction.INITIATE]
+            )
+          );
+
+          await fakeTreasuryBuybackStrategy.nftTokenId.returns(
+            BigNumber.from(1)
+          );
+
+          await fakeTreasuryBuybackStrategy.getAmountsFromPositionLiquidity.returns(
+            [BigNumber.from(1), BigNumber.from(1)]
+          );
+        });
+
+        context("when buyback not complete and under timeLimit", async () => {
+          it("should return false", async () => {
+            const upKeepStatus = await keepers02.checkUpkeep("0x");
+            expect(upKeepStatus[0]).to.be.false;
+          });
+        });
+
+        context("when buyback complete", async () => {
+          it("should return true with terminate action", async () => {
+            await fakeTreasuryBuybackStrategy.getAmountsFromPositionLiquidity.returns(
+              [BigNumber.from(0), BigNumber.from(1)]
+            );
+
+            const upKeepStatus = await keepers02.checkUpkeep("0x");
+            expect(upKeepStatus[0]).to.be.true;
+            expect(upKeepStatus[1]).to.be.eq(TreasuryBuybackAction.TERMINATE);
+          });
+        });
+
+        context("when pass timeLimit ", async () => {
+          it("should return true with terminate action", async () => {
+            await increaseTimestamp(BigNumber.from(TIME_LIMIT).add(1));
+
+            await advanceBlock();
+
+            const upKeepStatus = await keepers02.checkUpkeep("0x");
+            expect(upKeepStatus[0]).to.be.true;
+            expect(upKeepStatus[1]).to.be.eq(TreasuryBuybackAction.TERMINATE);
+          });
         });
       });
     });
-
-    // context("when alpaca not pass trigger wei", async () => {
-    // });
-    //     context("when remaining is 0", async () => {
-    //       it("should return true with correct data", async () => {
-    //         await keepers.setSlippageBps("10");
-    //         fakeToken.balanceOf.returns(TRIGGER_WEI);
-    //         fakeRevenueTreasury.router.returns(fakeSwapLikeRouter.address);
-    //         fakeRevenueTreasury.splitBps.returns(5000);
-    //         fakeRevenueTreasury.remaining.returns("0");
-    //         fakeRevenueTreasury.getRewardPath.returns([
-    //           fakeToken.address,
-    //           fakeToken.address,
-    //         ]);
-    //         fakeRevenueTreasury.getVaultSwapPath.returns([
-    //           fakeToken.address,
-    //           fakeToken.address,
-    //         ]);
-    //         fakeSwapLikeRouter.getAmountsOut.returns([
-    //           0,
-    //           ethers.utils.parseEther("5000"),
-    //         ]);
-    //         const upKeepStatus = await keepers.checkUpkeep("0x");
-    //         expect(upKeepStatus[0]).to.be.true;
-    //         expect(upKeepStatus[1]).to.eq(
-    //           ethers.utils.defaultAbiCoder.encode(
-    //             ["uint256", "uint256"],
-    //             ["0", ethers.utils.parseEther("4995")]
-    //           )
-    //         );
-    //       });
-    //     });
   });
 
-  // context("#performUpkeep", async () => {
-  //   context("when pendingAlpaca NOT pass the triggerWei", async () => {
-  //     it("should revert", async () => {
-  //       fakeToken.balanceOf.returns(TRIGGER_WEI.sub(1));
-  //       await expect(keepers.performUpkeep("0x")).to.be.revertedWith(
-  //         "RevenueTreasuryKeepers_NotPassTriggerWei()"
-  //       );
-  //     });
-  //   });
+  context("#performUpkeep", async () => {
+    context("when action is initiate", async () => {
+      it("should stamp initiateAt", async () => {
+        await keepers02.performUpkeep(
+          ethers.utils.defaultAbiCoder.encode(
+            ["uint256"],
+            [TreasuryBuybackAction.INITIATE]
+          )
+        );
 
-  //   context("when pendingAlpaca pass the triggerWei", async () => {
-  //     it("should forward token", async () => {
-  //       // Mock
-  //       fakeToken.balanceOf.returns(TRIGGER_WEI);
+        await fakeTreasuryBuybackStrategy.nftTokenId.returns(BigNumber.from(1));
 
-  //       // Interaction
-  //       await keepers.performUpkeep(
-  //         ethers.utils.defaultAbiCoder.encode(
-  //           ["uint256", "uint256"],
-  //           [ethers.utils.parseEther("4995"), ethers.utils.parseEther("4995")]
-  //         )
-  //       );
+        const initiateAt = await keepers02.initiateAt();
+        const block = await ethers.provider.getBlock("latest");
+        expect(initiateAt).to.eq(block.timestamp);
+      });
+    });
+    context("when action is terminate", async () => {
+      it("should reset initiateAt to 0", async () => {
+        await keepers02.performUpkeep(
+          ethers.utils.defaultAbiCoder.encode(
+            ["uint256"],
+            [TreasuryBuybackAction.INITIATE]
+          )
+        );
 
-  //       // Expect
-  //       expect(fakeRevenueTreasury.feedGrassHouse).to.have.been.calledOnce;
-  //     });
-  //   });
-  // });
+        expect(await keepers02.initiateAt()).to.not.eq(BigNumber.from(0));
+
+        await keepers02.performUpkeep(
+          ethers.utils.defaultAbiCoder.encode(
+            ["uint256"],
+            [TreasuryBuybackAction.TERMINATE]
+          )
+        );
+
+        expect(await keepers02.initiateAt()).to.eq(BigNumber.from(0));
+        expect(fakeRevenueTreasury02.stopBuybackStrategy).to.have.been
+          .calledOnce;
+        expect(fakeRevenueTreasury02.feedRevenueDistributor).to.have.been
+          .calledOnce;
+      });
+    });
+  });
 });
